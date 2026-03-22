ceb6e6dbd53bd1952c26e1d6c6065457dbb3cf09
grabsasha66@gmail.com <grabsasha66@gmail.com>
2026-03-22 13:34:01 +0200

    Вар. 10

--- LAB8/Form1.Designer.cs
+++ LAB8/Form1.Designer.cs
@@ -31,18 +31,22 @@
             textBox1 = new TextBox();
             button1 = new Button();
             label1 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            button2 = new Button();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(399, 118);
+            textBox1.Location = new Point(717, 56);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(110, 25);
             textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            button1.Location = new Point(426, 182);
+            button1.Location = new Point(744, 114);
             button1.Name = "button1";
             button1.Size = new Size(83, 25);
             button1.TabIndex = 1;
@@ -53,11 +57,49 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(282, 118);
+            label1.Font = new Font("Segoe UI", 12.2264156F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(559, 53);
             label1.Name = "label1";
-            label1.Size = new Size(111, 17);
+            label1.Size = new Size(152, 25);
             label1.TabIndex = 2;
             label1.Text = "Введіть своє ім'я:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12.2264156F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(77, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(139, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Введіть пароль:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(222, 61);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(110, 25);
+            textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.8679247F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label3.Location = new Point(249, 106);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 21);
+            label3.TabIndex = 5;
+            label3.Click += label3_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(249, 130);
+            button2.Name = "button2";
+            button2.Size = new Size(84, 25);
+            button2.TabIndex = 6;
+            button2.Text = "Перевірити";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
@@ -65,6 +107,10 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(884, 559);
+            Controls.Add(button2);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(button1);
             Controls.Add(textBox1);
@@ -80,5 +126,9 @@
         private TextBox textBox1;
         private Button button1;
         private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private Button button2;
     }
 }
--- LAB8/Form1.cs
+++ LAB8/Form1.cs
@@ -16,5 +16,36 @@ namespace LAB8
         {
             BackColor = Color.AntiqueWhite;
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string password = textBox2.Text;
+
+            bool hasUpper = false;
+            bool hasDigit = false;
+            bool length = password.Length >= 8;
+
+            foreach (char c in password)
+            {
+                if (char.IsUpper(c)) hasUpper = true; 
+                if (char.IsDigit(c)) hasDigit = true;
+            }
+
+            if (length && hasUpper && hasDigit)
+            {
+                label3.Text = "Пароль надійний!";
+                label3.ForeColor = Color.Green;
+            }
+            else
+            {
+                label3.Text = "Пароль не підходить!";
+                label3.ForeColor = Color.Red;
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
